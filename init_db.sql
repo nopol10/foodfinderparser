604b370b15e1e803603e71223ac380901b1e34b3
nopol10 <bobnewbie@gmail.com>
2016-10-20 12:36:44 +0800

    Updated DB Init script.

--- init_db.sql
+++ init_db.sql
@@ -1,3 +1,17 @@
 CREATE DATABASE foodfinder;
 CREATE USER 'foodfinder'@'localhost' IDENTIFIED BY 'foodfinder';
 grant all privileges on foodfinder.* to 'foodfinder'@'localhost';
+
+USE foodfinder;
+
+CREATE TABLE `restaurants` (
+      `restaurant_id` INT PRIMARY KEY AUTO_INCREMENT,
+      `restaurant_name` varchar(256) NOT NULL,
+      `country` varchar(256),
+      `web_rating` FLOAT,
+      `address` varchar(256),
+      `average_price` FLOAT,
+      `food_type` VARCHAR(256),
+      `source_website` varchar(256)
+) ENGINE=InnoDB;
+
